00847489bd4f14b641cd03d0d1926a8ed27234b1
Patrick Frantzen <patrick.frantzen@web.de>
2022-12-16 20:23:04 +0100

    add player rule description game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -11,7 +13,7 @@ export class GameComponent implements OnInit {
   currentCard: string = '';
   game: Game;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -19,12 +21,15 @@ export class GameComponent implements OnInit {
 
   newGame() {
     this.game = new Game();
+    console.log(this.game)
   }
 
   takeCard() {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop();
       this.pickCardAnimation = true;
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
     }
     setTimeout(() => {
       this.game.playedCards.push(this.currentCard);
@@ -32,5 +37,15 @@ export class GameComponent implements OnInit {
     }, 1000);
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
   
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name)
+      }
+    });
+  }
+
+
 }
